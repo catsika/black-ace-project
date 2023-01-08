3f991684389841aaec40c22e7d00bda089d6f5c9
Daemon <paakwesinunoo135@gmail.com>
2023-01-07 18:03:16 -0800

    added functionality to login and register

--- server/server.js
+++ server/server.js
@@ -2,13 +2,14 @@ const express = require('express')
 const dotenv = require('dotenv').config()
 const port = process.env.PORT 
 const connectDb = require('./Database/connect')
+const cors = require('cors')
 
 const app = express()
 connectDb()
 
 app.use(express.json())
 app.use(express.urlencoded({extended:false}))
-
+app.use(cors())
 app.use('/api/users',require('./Routes/userRoutes'))
 app.use('/api/feed',require('./Routes/feedRoutes'))
 app.use('/api/community',require('./Routes/communityRoutes'))
