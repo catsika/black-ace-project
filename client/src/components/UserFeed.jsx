edaa4cb916f02a5ea679b65bed1dc05ad9a887be
Daemon <paakwesinunoo135@gmail.com>
2023-01-09 21:53:09 -0800

    added profilepage

---
+++ client/src/components/UserFeed.jsx
@@ -0,0 +1,14 @@
+import {useState} from 'react'
+
+const UserFeed = ({feed}) => {
+const [userfeed,setUserFeed] = useState([])
+
+
+  return (
+    <div>
+ 
+    </div>
+  )
+}
+
+export default UserFeed
--- client/src/pages/ProfilePage.jsx
+++ client/src/pages/ProfilePage.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import { useNavigate } from 'react-router-dom';
 import { useState,useEffect} from 'react';
 
+
+
 export const Profile = () => {
   const [info,setInfo] = useState([])
   const navigate = useNavigate()
@@ -16,23 +18,32 @@ export const Profile = () => {
 }, [navigate])
 
   return (
-    <div>
+    <div className="profile-container">
       {console.log(info)}
 
-<div>
-  {info.username}
-  
-</div>
-<div className="">
-  {info.feed}
-</div>
-<div className="image">
+
+<div className="profile-image">
   <img src={info.profileImage} alt="" />
+</div>
+
+<div className="bio">
+  {info.Bio}
+</div>
+<div className="profile-username">
+  {info.username}
+</div>
+<div className="profile-birth">
+ {info.birth}
+</div>
+<div className="profile-city">
+  {info.city}
 </div>
 
 
       <button className="submit"  onClick={logout}>logout</button>
+
     </div>
+
   )
 }
 export default Profile
